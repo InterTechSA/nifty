bdd0207b3b6038395f7060be14db15d00184c64e
Thabang Kubheka <cartide09@gmail.com>
2024-06-17 19:17:03 +0200

    Finalised UI for nav

--- Frontend/nifty/src/Components/Navigation/Nav.js
+++ Frontend/nifty/src/Components/Navigation/Nav.js
@@ -1,52 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-
-const Nav = styled.nav`
-  background: #002244;
-  height: 80px;
-  display: flex;
-  justify-content: space-between;
-  padding: 0 20px;
-  align-items: center;
-`;
-
-const NavLink = styled(Link)`
-  color: #fff;
-  text-decoration: none;
-  padding: 12px;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background: #037B4A;
-    border-radius: 5px;
-  }
-`;
-
-const NavLogo = styled(Link)`
-  color: #fff;
-  text-decoration: none;
-  font-size: 2rem;
-  font-weight: 700;
-`;
-
-const NavMenu = styled.div`
-  display: flex;
-  align-items: center;
-`;
+import { Nav, NavLink, Container } from '../../Common/Styles/Styles'
 
 const Navbar = () => {
 
   return (
     <Nav>
-      <NavLogo to="/">Nifty</NavLogo>
-      <NavMenu>
+      <NavLink to="/">Nifty</NavLink>
+      <Container>
         <NavLink to="/signin">Sign In</NavLink>
         <NavLink to="/signup">Sign Up</NavLink>
-      </NavMenu>
+      </Container>
     </Nav>
   );
 };
